30ae742f499eeb921a846b154d3948f6133784f9
YOOJS1205 <junsang8921@gmail.com>
2022-11-20 01:09:57 +0900

    Feat: 라우터 세팅

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,5 +1,26 @@
+// 라이브러리 import
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// 컴포넌트 import
+import Home from './Home';
+import My from './My';
+import Pair from './Pair';
+import Question from './Question';
+import Guide from './Guide';
+import Result from './Result';
 
 export default function Router() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/my" element={<My />} />
+        <Route path="/pair" element={<Pair />} />
+        <Route path="/question/:id" element={<Question />} />
+        <Route path="/guide/:id" element={<Guide />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
