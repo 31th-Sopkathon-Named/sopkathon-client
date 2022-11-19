0833e5dc796d8df13398761b5710d62b48b6ae15
YOOJS1205 <junsang8921@gmail.com>
2022-11-20 01:59:35 +0900

    Feat: Recoil 세팅

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import Router from './pages/Router';
 import GlobalStyle from './styles/globalStyle';
@@ -6,11 +7,11 @@ import theme from './styles/theme';
 
 export default function App() {
   return (
-    <>
+    <RecoilRoot>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Router />
       </ThemeProvider>
-    </>
+    </RecoilRoot>
   );
 }
--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+
+import { selectAryState } from '../core/recoil/selectAryState';
 
 export default function Question() {
+  const [selectList, setSelectList] = useRecoilState(selectAryState);
+  console.log(selectList, setSelectList);
   return <div></div>;
 }
