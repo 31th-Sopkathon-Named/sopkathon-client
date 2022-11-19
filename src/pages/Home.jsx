c52980bde6d0236b3673a68526eaff039e7c59f2
YOOJS1205 <junsang8921@gmail.com>
2022-11-20 05:32:57 +0900

    Feat: 버튼 컴포넌트, 메인 페이지

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { userAPI, matchAPI, evaluateAPI } from '../core/api/userAPI';
+import styled from 'styled-components';
+
+import { ReactComponent as MainLogo } from '../assets/logos/mainLogo.svg';
+import ClockImg from '../assets/images/clock.png';
+
+// import Container from '../components/common/Container';
+import Button from '../components/common/Button';
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleStartButton = useCallback(() => {
+    navigate('/my');
+  }, []);
   useEffect(() => {
     userAPI('Yoo', 8921);
     matchAPI('Wook', 1234);
     evaluateAPI(sessionStorage.getItem('toId'), sessionStorage.getItem('fromId'), [1, 2, 3, 4]);
   }, []);
-  return <div></div>;
+  return (
+    <StyledContainer>
+      <MainLogo />
+      <img src={ClockImg} />
+      <StyledMainText>2H</StyledMainText>
+      <StyledSubText>
+        사람의 마음의 변화는
+        <br />
+        2시간 2병 내에 결정된다.
+      </StyledSubText>
+      <Button text="시작하기" enabled={true} onClick={handleStartButton} />
+    </StyledContainer>
+  );
 }
+
+const StyledContainer = styled.section`
+  /* background-image: url('../assets/images/backgroundImg.png'); */
+  background-color: black;
+  text-align: center;
+  padding-top: 143px;
+  height: 100vh;
+
+  img {
+    display: block;
+    margin: 17px auto 102px;
+  }
+`;
+
+const StyledMainText = styled.p`
+  color: white;
+  font-size: 12px;
+`;
+
+const StyledSubText = styled.p`
+  color: white;
+  margin: 40px 0 44px;
+  font-size: 12px;
+  line-height: 20px;
+`;
